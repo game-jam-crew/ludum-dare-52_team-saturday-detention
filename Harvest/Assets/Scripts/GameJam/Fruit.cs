50c9e5c0d1ea3ab3b8ed634b553cbfc58897bc76
Joey Shipley <joey.shipley@gmail.com>
2023-01-06 23:25:37 -0600

    Fruit falls from tree
    Co-authored-by: Ian Gagorik <ian.gagorik@gmail.com>

--- Harvest/Assets/Scripts/GameJam/Fruit.cs
+++ Harvest/Assets/Scripts/GameJam/Fruit.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -6,13 +7,56 @@ namespace GameJam
     public class Fruit : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField] bool isOnTree;
+        [SerializeField] bool isFalling;
+        [SerializeField] bool isOnGround;
+        
         [SerializeField] bool isBad;
+        
+        [SerializeField] float fallSpeed = 450.0f;
+        [SerializeField] float fallDistance = 350.0f;
+        
+        Vector2 startingPosition;
 
         void Start()
         {
             isOnTree = true;
+            isFalling = false;
+            isOnGround = false;
+            
             isBad = false;
+            
+            startingPosition = transform.position;
         }
+        
+        void Update()
+        {
+            // TODO: if !isOnTree, start fall coroutine.
+            if(!isOnTree && !isFalling && !isOnGround)
+            {
+                StartCoroutine(fall());
+            }
+        }
+        
+        IEnumerator fall()
+        {
+            isFalling = true;
+            var distanceTraveled = 0.0f;
+
+            while(distanceTraveled < fallDistance)
+            {
+                distanceTraveled += fallSpeed * Time.deltaTime;
+                Debug.Log($"Fall distance this frame = { distanceTraveled }");
+                
+                transform.position = new Vector2(startingPosition.x, startingPosition.y - distanceTraveled);
+                
+                yield return null;
+            }
+            
+            Debug.Log("Done, the fruit is in the dirt.");
+            isFalling = false;
+            isOnGround = true;
+        }
+            
         
         public void OnPointerClick(PointerEventData eventData)
         {

361c86f39860b809717ec68f92bce134d211e7fa
Joey Shipley <joey.shipley@gmail.com>
2023-01-06 21:26:06 -0600

    Adds scene transition.
    Co-authored-by: Ian Gagorik <ian.gagorik@gmail.com>

--- Harvest/Assets/Scripts/GameJam/Scenes/GameSceneController.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace GameJam.Scenes
-{
-    public class GameSceneController : MonoBehaviour
-    {
-    }
-}
--- Harvest/Assets/Scripts/GameJam/System/View/GameViewManager.cs
+++ Harvest/Assets/Scripts/GameJam/System/View/GameViewManager.cs
@@ -1,6 +1,12 @@
+using UnityEngine.SceneManagement;
+
 namespace GameJam.System.View
 {
     public class GameViewManager : SingletonMonoBehaviour<GameViewManager>
     {
+        public void OpenGameScene()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
 }

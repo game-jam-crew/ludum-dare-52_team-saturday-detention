b9392f11d678556bb678178c93c47a091212a55f
Joey Shipley <joey.shipley@gmail.com>
2023-01-06 20:17:36 -0600

    quick checkin

--- Harvest/Assets/Scripts/GameJam/Scenes/StartSceneController.cs
+++ Harvest/Assets/Scripts/GameJam/Scenes/StartSceneController.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GameJam.Scenes
 {
     public class StartSceneController : MonoBehaviour
     {
+        [SerializeField] Button _startGameButton;
+        
+        void Awake()
+        {
+            // _startGameButton.onClick += startGameHandler;
+        }
+        
+        void startGameHandler()
+        {
+        }
     }
 }
